ec6def8be7a5b7fe657b5bfab68fa1d024c52cab
vatsroshan333@gmail.com <vatsroshan333@gmail.com>
2025-12-08 07:22:19 +0000

    update prod

--- models/mart-prod/prod_dim_location_details.sql
+++ models/mart-prod/prod_dim_location_details.sql
@@ -1,4 +1,10 @@
-{{ config(materialized='table') }}
+{{ 
+  config(
+    materialized='table',
+    schema= 'SALES' if target.name == 'prod' else 'RVATS'
+  ) 
+}}
+
 WITH dim_location AS ( 
     SELECT * FROM {{ ref('int_dim_location_details') }}
 )
